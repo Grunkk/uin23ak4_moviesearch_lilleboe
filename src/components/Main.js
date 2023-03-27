e534ef8e1402ac8389947cc195aa73095401593a
Grunkk <sigurd6260@gmail.com>
2023-03-27 20:56:53 +0200

    OMG DET FUNKER

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,33 +1,58 @@
 import { useEffect, useState } from "react"
 import MovieCard from "./MovieCard"
 
-export default function Main(){
+export default function Main({props}){
 
-    const [movies, setMovies] = useState([])
+    const [printmovies, setPrint] = useState([])
+    const [input, setInput] = useState("james-bond")
 
 
     const getMovies = async() =>{
-        const response = await fetch('http://www.omdbapi.com/?i=tt3896198&apikey=80218af1&type=movie&s=james-bond')
+        if(input.length > 3){
+
+        const response = await fetch(`http://www.omdbapi.com/?apikey=80218af1&type=movie&s=${input.toLowerCase().replace(" ", "-")}`)
         const data = await response.json()
         console.log("data search", data?.Search)
-        setMovies(data?.Search)
         
+        const moviesearch = data?.Search
+
+        const resultat = moviesearch.map((items => items?.imdbID))
+
+        const realmovies = await Promise.all(resultat.map(id => (
+            fetch(`http://www.omdbapi.com/?apikey=80218af1&type=movie&i=${id}`)
+        )))
+
+        const realrealmovies = await Promise.all(realmovies.map(movieresponse => (
+            movieresponse.json()
+        )))
+        
+        setPrint(realrealmovies)
 
         console.log("data", data)
+    }else{
+        alert("Du kan ikke søke med mindre enn 3 tegn")
+
+        setInput("james-bond")
+    }
     }
 
     useEffect(() =>{
         getMovies()
     },[])
 
-   console.log("movie", movies)
-
+    console.log("printmovies", printmovies)
+    console.log("input", input)
 
     return(
         //<h3>hehe</h3>
         <>
         <h2>James bond filmer</h2>
-        {movies?.map((item, index) =>(
+    
+         <input type="text" placeholder="james bond.." name="search" onChange={e => setInput(e.target.value)} />
+            <button onClick={getMovies}>Søk</button>
+            
+
+        {printmovies?.map((item, index) =>(
             <MovieCard key={index} title={item?.Title} img={item?.Poster} />
         ))}
         </>
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -2,7 +2,9 @@ export default function MovieCard({title, img}){
     return (
         <article>
             <h3>{title}</h3>
-            <img src={img} alt={title} />
+            <img src={img == "N/A" ? "https://cdn.pixabay.com/photo/2016/11/03/14/18/stamp-1794352_960_720.png" : img} alt={title} />
+            
         </article>
+        //bilde er hentet fra https://cdn.pixabay.com/photo/2016/11/03/14/18/stamp-1794352_960_720.png
     )
 }
